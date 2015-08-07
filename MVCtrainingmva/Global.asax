c6698edcf53285c7de9da7b762f14cd3d7fe1606
unknown <pranavm@Pranav-PC.in.freshview>
2015-08-07 11:31:48 +1000

    created project file

---
+++ MVCtrainingmva/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVCtrainingmva.MvcApplication" Language="C#" %>
---
+++ MVCtrainingmva/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVCtrainingmva.Startup))]
+namespace MVCtrainingmva
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
