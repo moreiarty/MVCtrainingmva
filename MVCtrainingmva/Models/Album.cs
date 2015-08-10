62aae7ff12394415d1f3baf14830eafdc9e7c834
unknown <pranavm@Pranav-PC.in.freshview>
2015-08-10 10:00:53 +1000

    Finished Lesson 1, and started Lesson 2

---
+++ MVCtrainingmva/Models/Album.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MVCtrainingmva.Models
+{
+    class Album
+    {
+        public int AlbumID { get; set; }
+        public string Title { get; set; }
+        public Artist Artist { get; set; }
+    }
+}
